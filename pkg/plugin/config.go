8c5d0d4c65c237909f3c6aa3f0cba071927c4a72
Ryo Kitagawa <kitadrum50@gmail.com>
2023-06-23 16:05:08 +0900

    Open browser

--- pkg/plugin/config.go
+++ pkg/plugin/config.go
@@ -10,9 +10,14 @@ import (
 type ConfigFlags struct {
 	Kubernetes *genericclioptions.ConfigFlags
 
+	// Log time duration
 	Duration time.Duration
 
+	// Add Cloud logging filter. https://cloud.google.com/logging/docs/view/building-queries
 	Filter string
+
+	// Open web browser
+	Web bool
 }
 
 func NewConfigFlags() *ConfigFlags {
@@ -30,6 +35,7 @@ func (c *ConfigFlags) AddFlags(flags *pflag.FlagSet) {
 
 	flags.DurationVar(&c.Duration, "duration", 30*time.Minute, "Log time duration")
 	flags.StringVar(&c.Filter, "filter", "", "Add Cloud logging filter. https://cloud.google.com/logging/docs/view/building-queries")
+	flags.BoolVar(&c.Web, "web", false, "Open web browser")
 }
 
 func toPtr[T any](s T) *T {
